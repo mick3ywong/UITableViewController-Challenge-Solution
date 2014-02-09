fbbf3898694a4a23152b3b85ec0870501eca85f2
Mike Siew Choong Wong <mick3ywong@Mikes-MacBook-Pro.local>
2014-02-09 18:47:11 +0800

    solved challenge with extra credit

--- TVTableViewController.h
+++ TVTableViewController.h
@@ -1,5 +1,5 @@
 //
-//  TVViewController.h
+//  TVTableViewController.h
 //  UITableViewController Challenge Solution
 //
 //  Created by Mike Siew Choong Wong on 2/9/14.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface TVViewController : UIViewController
+@interface TVTableViewController : UITableViewController
 
 @end
